c3a4a9aa78488ae950780e247756202ece7af2a8
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2023-07-28 11:09:03 +0100

    Go back to til package name for now

--- til/core.py
+++ til/core.py
@@ -3,19 +3,19 @@
 # %% auto 0
 __all__ = ['foo', 'say_hello', 'HelloSayer']
 
-# %% ../nbs/00_core.ipynb 3
+# %% ../nbs/00_core.ipynb 4
 def foo(): pass
 
-# %% ../nbs/00_core.ipynb 4
+# %% ../nbs/00_core.ipynb 5
 def say_hello(to):
     "Say hello to somebody"
     return f'Hello {to}!'
 
-# %% ../nbs/00_core.ipynb 9
+# %% ../nbs/00_core.ipynb 10
 class HelloSayer:
     "Say hello to `to` using `say_hello`"
     def __init__(self, to): self.to = to
         
     def say(self):
-        "Do the saying"
+        "Say hello to `to` using `say_hello`"
         return say_hello(self.to)
