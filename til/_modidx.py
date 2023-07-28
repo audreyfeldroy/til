764d89ec18b49416ce6d27a467f3fa47bc8b95da
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2023-07-28 09:55:16 +0100

    Through https://nbdev.fast.ai/tutorials/tutorial.html#add-links-with-backticks

--- til/_modidx.py
+++ til/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://audreyfeldroy.github.io',
                 'git_url': 'https://github.com/audreyfeldroy/til',
                 'lib_path': 'til'},
-  'syms': {'til.core': {'til.core.foo': ('core.html#foo', 'til/core.py'), 'til.core.say_hello': ('core.html#say_hello', 'til/core.py')}}}
+  'syms': { 'til.core': { 'til.core.HelloSayer': ('core.html#hellosayer', 'til/core.py'),
+                          'til.core.HelloSayer.__init__': ('core.html#hellosayer.__init__', 'til/core.py'),
+                          'til.core.HelloSayer.say': ('core.html#hellosayer.say', 'til/core.py'),
+                          'til.core.foo': ('core.html#foo', 'til/core.py'),
+                          'til.core.say_hello': ('core.html#say_hello', 'til/core.py')}}}
