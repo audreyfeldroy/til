a1db4a35617da3d2804e2caf0de0921933f12305
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2024-08-01 19:59:46 +0100

    Delete todos (through 43:30)

--- apps/getting_started_w_fh/03_db.py
+++ apps/getting_started_w_fh/03_db.py
@@ -3,7 +3,8 @@ from fasthtml.common import *
 def render(todo):
     tid = f'todo-{todo.id}'
     toggle = A('Toggle', hx_get=f'/toggle/{todo.id}', target_id=tid)
-    return Li(toggle, todo.title + (" ✅" if todo.done else ""), id=tid)
+    delete = A('Delete', hx_delete=f'/{todo.id}', hx_swap='outerHTML', target_id=tid)
+    return Li(toggle, delete, todo.title + (" ✅" if todo.done else ""), id=tid)
 
 app,rt,todos,Todo = fast_app('todos.db', live=True, render=render,
                              id=int, title=str, done=bool, pk='id')
@@ -13,6 +14,9 @@ def get():
     # todos.insert(Todo(title="Make dinner", done=False))
     # items = [Li(o) for o in todos()]
     return Titled("Todos", Ul(*todos()))
+
+@rt('/{tid}')
+def delete(tid:int): todos.delete(tid)
 
 @rt("/toggle/{tid}")
 def get(tid:int):
