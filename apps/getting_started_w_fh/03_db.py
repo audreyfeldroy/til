9e34adf2c1dc9eb49a1b453b05da89ef01d4fdf0
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2024-08-01 16:29:08 +0100

    Get Todo() working

--- apps/getting_started_w_fh/03_db.py
+++ apps/getting_started_w_fh/03_db.py
@@ -6,7 +6,7 @@ app,rt,todos,Todo = fast_app('todos.db', live=True,
 
 @rt("/")
 def get():
-    todos.insert(Todo("Pick up Uma", done=False))
+    todos.insert(Todo(title="Pick up Uma", done=False))
     items = todos()
     return Titled("Todos", Div(*items))
 
